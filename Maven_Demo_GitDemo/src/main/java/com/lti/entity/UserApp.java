3e46a50107682941324bdac16feebaef6b392a1f
Kamlesh <kamaleshmadan4@gmail.com>
2022-07-14 12:17:43 +0530

    first commit on Thur 14th July 22 create new project

---
+++ Maven_Demo_GitDemo/src/main/java/com/lti/entity/UserApp.java
@@ -0,0 +1,9 @@
+package com.lti.entity;
+
+public class UserApp {
+	public static void main(String[] args) {
+		Calculator c=new Calculator();
+			int r=c.add(100,200);
+			System.out.println(r);
+	}
+}
